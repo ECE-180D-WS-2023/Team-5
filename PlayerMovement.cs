188d3c994786cdfa2c1935d78bd640d563f0439f
Sraavya Pradeep <46724697+SraavyaPradeep@users.noreply.github.com>
2023-03-21 22:23:24 -0700

    Add files via upload

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 // https://www.youtube.com/watch?v=u8tot-X_RBI for code on 2D top down movement
@@ -10,6 +11,8 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
 
     private Vector2 moveDirection;
+
+    public List<string> faceTrack;
 
     // Update is called once per frame
     void Update()
@@ -22,16 +25,38 @@ public class PlayerMovement : MonoBehaviour
         Move();
     }
 
+    string readFile(){
+        var lines = System.IO.File.ReadAllLines("/Users/sraavyapradeep/180DA-WarmUp/facetracking/facetracking.txt");
+        // foreach (var line in lines){
+        //     var data = line;
+        //     faceTrack.Add(data);
+        //     print(data);
+        //     break;
+        // }
+        return(lines[lines.Length - 1]);
+    }
+
     void ProcessInputs()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
-        moveDirection = new Vector2(moveX, moveY).normalized;
+        //moveDirection = new Vector2(moveX, moveY).normalized;
     }
     // adding a velocity to the player
     void Move()
     {
+        string s = readFile();
+        int val = System.Convert.ToInt32(s);
+        print(val);
+        if (val > 550){
+            moveDirection = new Vector2(-1, 0).normalized;
+        } else if (val < 450) {
+            moveDirection = new Vector2(1, 0).normalized;
+        } else {
+            moveDirection = new Vector2(0, 0).normalized;
+        }
+        
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 }
