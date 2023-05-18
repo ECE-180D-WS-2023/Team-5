f0b0032ed02a2a8a0a0c0e05a2afa87bbda0ec16
SeungWoo100 <76974257+SeungWoo100@users.noreply.github.com>
2023-05-18 15:58:36 -0700

    Add files via upload

--- MainMenu.cs
+++ MainMenu.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// 0 : Main Menu
+// 1 : Single Player Mode
+// 2 : Explanation (Intro)
+// 3 : Tutorial
+// 4 : Multiplayer Mode
 public class MainMenu : MonoBehaviour
 {
+    public void ReturnMainFromIntro()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2); //Intro -> Main Menu
+    }
+    public void ReturnMainFromTuto()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3); //Main Menu -> Intro
+    }
     public void SinglePlayer()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //goes to next scene index which from main menu is single player
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Main menu -> Single
+    }
+    public void Intro()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2); //Main Menu -> Intro
     }
     public void MultiPlayer()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2); //goes to scene two scenes after main menu index
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4); //Main Menu -> Multi
     }
+    public void IntroToTutorial()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Intro -> Tutorial
+    }
+  
 }
